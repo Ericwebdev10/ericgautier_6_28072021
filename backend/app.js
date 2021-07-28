05f8e9accbede37c682716878760787967d6c062
ericwebdev <ericwebdev10@gmail.com>
2021-07-28 17:28:25 +0200

    Add Dotenv and change user access

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,7 @@
+//https://www.npmjs.com/package/dotenv
+//Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env. Storing configuration in the environment separate from code is based on The Twelve-Factor App methodology.
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -5,8 +9,7 @@ const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
-
-mongoose.connect('mongodb+srv://eric:2514@cluster0.en309.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS +'@cluster0.en309.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -14,6 +17,7 @@ mongoose.connect('mongodb+srv://eric:2514@cluster0.en309.mongodb.net/myFirstData
 
 const app = express();
 
+//Cross Origin Resource Sharing (CORS) is a W3C standard that allows a server to relax the same-origin policy. Using CORS, a server can explicitly allow some cross-origin requests while rejecting others.
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
